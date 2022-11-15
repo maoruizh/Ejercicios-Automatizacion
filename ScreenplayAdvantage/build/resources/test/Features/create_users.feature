7c7e0ead91a72f709ac380e7351c71374db0a607
Mao Ruiz <51135669+maoruizh@users.noreply.github.com>
2022-11-15 11:48:43 -0500

    Ejercicio nuevo de Screenplay

---
+++ ScreenplayAdvantage/build/resources/test/Features/create_users.feature
@@ -0,0 +1,9 @@
+Feature: successfully create users
+  Me as course automation
+  I need to create a users
+  To validate that users are successfully created
+
+  Scenario: create an user successfully
+    Given I want to go to the page of advantage demo
+    When I enter the informationn to create the user
+    Then I validate the correct user creation
---
+++ ScreenplayAdvantage/src/test/java/co/com/empresamao/areait/advantage/stepdefinitions/CreateUsersSteps.java
@@ -0,0 +1,20 @@
+package co.com.empresamao.areait.advantage.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CreateUsersSteps {
+    @Given("^I want to go to the page of advantage demo$")
+    public void iWantToGoToThePageOfAdvantageDemo() {
+
+    }
+
+    @When("^I enter the informationn to create the user$")
+    public void iEnterTheInformationnToCreateTheUser() {
+    }
+
+    @Then("^I validate the correct user creation$")
+    public void iValidateTheCorrectUserCreation() {
+    }
+}
