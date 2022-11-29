6c127bbe6da4b6f8561cf4c1c22e1e8f5501890f
Mao Ruiz <51135669+maoruizh@users.noreply.github.com>
2022-11-28 19:56:17 -0500

    Se termina la prueba de Utest

--- Utest/src/main/java/co/com/empresamao/areait/utest/tasks/LoginUsers.java
+++ Utest/src/main/java/co/com/empresamao/areait/utest/tasks/LoginUsers.java
@@ -1,11 +1,14 @@
 package co.com.empresamao.areait.utest.tasks;
 
 
+import co.com.empresamao.areait.utest.userinterfaces.LoginPage;
+import javafx.scene.control.Tab;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.SendKeys;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 
 
@@ -28,13 +31,36 @@ public class LoginUsers implements Task {
                 Click.on(TXT_LOGIN_LANGUAGE),
                 Enter.theValue("Spanish").into(TXT_LOGIN_LANGUAGE),
                 Click.on(BTN_NEXT),
-                Enter.theValue("Sabaneta, Antioquia, Colombia").into(TXT_CITY),
+                //Enter.theValue("Sabaneta, Antioquia, Colombia").into(TXT_CITY),
                 Enter.theValue("055450").into(TXT_POSTAL_CODE),
-                Enter.theValue("Colombia").into(TXT_COUNTRY)
-
+                Click.on(TXT_COUNTRY),
+                Click.on(LST_COUNTRY),
+                //Enter.theValue("Colombia").into(TXT_COUNTRY),
+                Click.on(BTN_NEXT_DEVICES),
+                Click.on(BTN_SO),
+                Click.on(LST_SO),
+                Click.on(BTN_VERSION),
+                Click.on(LST_VERSION),
+                Click.on(BTN_OS_LANGUAGE),
+                Click.on(LST_OS_LANGUAGE),
+                Click.on(BTN_MOBILE_DEVICE),
+                Click.on(LST_MOBILE_DEVICE),
+                WaitUntil.the(BTN_MODEL, isVisible()).forNoMoreThan(15).seconds(),
+                Click.on(BTN_MODEL),
+                Click.on(LST_MODEL),
+                WaitUntil.the(BTN_OPERATING_SYSTEM, isVisible()).forNoMoreThan(15).seconds(),
+                Click.on(BTN_OPERATING_SYSTEM),
+                Click.on(LST_OPERATING_SYSTEM),
+                Click.on(BTN_NEXT_LAST_STEP),
+                Enter.theValue("Asdfghj1234567*4").into(TXT_PASSWORD),
+                Enter.theValue("Asdfghj1234567*4").into(TXT_CONFIRM_PASSWORD),
+                Click.on(CHK_TERM_OF_USE),
+                Click.on(CHK_PRIVACY_SETTING),
+                Click.on(BTN_COMPLETE_SETUP)
 
         );
     }
+
     public static LoginUsers loginUsers() {
 
         return instrumented(LoginUsers.class);
