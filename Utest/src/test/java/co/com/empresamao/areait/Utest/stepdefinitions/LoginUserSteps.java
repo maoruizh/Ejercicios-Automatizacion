22698e5cdac78d93f724669ece7c969217e78af9
Mao Ruiz <51135669+maoruizh@users.noreply.github.com>
2022-12-12 18:54:53 -0500

    se crea el driver dinamico
    se elimina el chromedriver
    no se usa mas la clase mywebdriver
    y se cambian unas lineas en el step definition para que el test descargue el driver del navegador actualizado y lo ejecuta solo

--- Utest/src/test/java/co/com/empresamao/areait/Utest/stepdefinitions/LoginUserSteps.java
+++ Utest/src/test/java/co/com/empresamao/areait/Utest/stepdefinitions/LoginUserSteps.java
@@ -1,7 +1,6 @@
 package co.com.empresamao.areait.Utest.stepdefinitions;
 
 import co.com.empresamao.areait.utest.tasks.LoginUsers;
-import co.com.empresamao.areait.utest.utils.MyDriversWeb;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -11,13 +10,23 @@ import net.serenitybdd.screenplay.actors.OnStage;
 
 import static co.com.empresamao.areait.utest.utils.Constants.*;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.screenplay.actions.Open;
 
 public class LoginUserSteps {
+    @Managed(driver = "chrome")//si quieres otro navegador se puede colocar simplemente la palabra
+    protected WebDriver hisBrowser;
+
     @Given("^I want to go to the login page of Utest$")
     public void iWantToGoToTheLoginPageOfUtest() {
-        OnStage.setTheStage(Cast.ofStandardActors());
+        WebDriverManager.chromedriver().setup();// si quiero abrirlo con otro navegador lo cambio a ca donde dice chromedriver por ejemplo: firefoxdriver
+        OnStage.setTheStage(new Cast());
         OnStage.theActorCalled(ACTOR_PAGE);
-        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(MyDriversWeb.web().inThePage(URL)));
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(hisBrowser));
+        OnStage.theActorInTheSpotlight().wasAbleTo(Open.url(URL));
+        hisBrowser.manage().window().maximize();
     }
 
     @When("^I enter the information to login a user$")
